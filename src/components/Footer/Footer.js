0de35b2015285a3a1946ad690699028d3089e22c
Enes <120328716+eneswastaken@users.noreply.github.com>
2024-05-30 18:18:15 +0300

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,10 +3,10 @@ import './Footer.css'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/rajshekhar26/cleanfolio'
+      href='https://www.ensomg.net.tr/'
       className='link footer__link'
     >
-      Created By Raj Shekhar
+      Created By Ens
     </a>
   </footer>
 )
